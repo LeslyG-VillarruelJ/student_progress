be6616b2457b184aca6bef48e0daeb33588d8e67
Lesly Villarruel <156205480+LeslyG-VillarruelJ@users.noreply.github.com>
2025-07-25 15:46:07 -0500

    changes in install

--- db/install.php
+++ db/install.php
@@ -5,22 +5,15 @@ defined('MOODLE_INTERNAL') || die();
 function xmldb_block_student_progress_install() {
     global $DB;
 
-    // Asegura que el bloque está registrado.
-    $blockname = 'student_progress';
-
-    // Obtener todos los cursos excepto el curso sitio (id = 1).
-    $courses = $DB->get_records_select('course', 'id != 1');
+    // Obtener todos los cursos (excluyendo el curso sitio - id = 1)
+    $courses = $DB->get_records_select('course', 'id > 1');
 
     foreach ($courses as $course) {
+        // Preparar bloque en la región lateral (side-pre)
         $page = new moodle_page();
         $page->set_context(context_course::instance($course->id));
         $page->set_pagelayout('course');
-        $page->set_url('/course/view.php', ['id' => $course->id]);
-
-        // Asegura que no esté ya agregado.
-        if (!block_is_on_page($blockname, $page)) {
-            // Agrega el bloque automáticamente a la región lateral derecha.
-            blocks_add_block($blockname, $page, 'side-post', 0, false);
-        }
+        $page->set_url(new moodle_url('/course/view.php', ['id' => $course->id]));
+        $page->blocks->add_block('student_progress', 'side-pre', 0, false);
     }
 }
